db09b701b756433c855d103d891d3a577834c850
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-09-05 12:51:41 +0300

    chore: disable_implicity_numpy_multithreading, do not override externally set variables

--- src/dag_modelling/tools/disable_implicit_numpy_multithreading.py
+++ src/dag_modelling/tools/disable_implicit_numpy_multithreading.py
@@ -1,7 +1,12 @@
-import os
+from os import environ
 
 # Force single threaded mode. Must be done before numpy is loaded.
+# Overrides variables only if they are not set.
 num_threads = "1"
-os.environ["OMP_NUM_THREADS"] = num_threads
-os.environ["OPENBLAS_NUM_THREADS"] = num_threads
-os.environ["MKL_NUM_THREADS"] = num_threads
+environment_variables = "OMP_NUM_THREADS" "OPENBLAS_NUM_THREADS" "MKL_NUM_THREADS"
+
+for var_name in environment_variables:
+    if var_name in environ:
+        continue
+
+    environ[var_name] = num_threads
