dcccf0c1cc74f0be659a312fd47a811abdb3a998
Zavadskyi Vitalii <zavadskiy.vs@phystech.edu>
2025-09-17 10:23:04 +0300

    clear: test

--- tests/tools/test_make_fcn.py
+++ tests/tools/test_make_fcn.py
@@ -9,14 +9,14 @@ from dag_modelling.parameters import Parameters
 from dag_modelling.plot.graphviz import savegraph
 
 
-@mark.parametrize("par_names,mapper", (
+@mark.parametrize("par_dict,mapper", (
     ({}, None),
     ({"a": 2.0, "b.IDX1": 3.0}, {"b.IDX1": "par1"}),
     ({"a": 2.0}, {"a": "par0"}),
     ({"b.IDX1": 3.0}, {"a": "par0"}),
 ))
 @mark.parametrize("pass_output", (False, True))
-def test_make_fcn_safe(test_name, par_names, mapper, pass_output, output_path: str):
+def test_make_fcn_safe(test_name, par_dict, mapper, pass_output, output_path: str):
     n = 10
     x = arange(n, dtype="d")
     vals_in = [1.0, 2.0]
@@ -37,44 +37,44 @@ def test_make_fcn_safe(test_name, par_names, mapper, pass_output, output_path: s
     LF = make_fcn(
         f.outputs[0] if pass_output else f,
         storage["parameters.all"],
-        par_names=par_names,
+        par_names=par_dict,
         mapper=mapper,
         safe=True,
     )
 
-    if par_names:
+    if par_dict:
         with raises(KeyError):
             LF(non_existent=12)
 
         with raises(RuntimeError):
-            LF(*[value for value in par_names.values()], non_existent=12)
+            LF(*[value for value in par_dict.values()], non_existent=12)
 
-    res1 = LF(*[value for value in par_names.values()])
+    res1 = LF(*[value for value in par_dict.values()])
     res2 = LF()
 
     if mapper:
-        res3 = LF(**{mapper.get(parname, parname): parvalue for parname, parvalue in par_names.items()})
+        res3 = LF(**{mapper.get(parname, parname): parvalue for parname, parvalue in par_dict.items()})
         assert all(res1 == res3)
 
     # pars equal inital values
     assert A.value == vals_in[0]
     assert B.value == vals_in[1]
     # new result differs from the result of LF
-    assert all(res1 == (par_names.get("a", vals_in[0]) * x + par_names.get("b.IDX1", vals_in[1])))
+    assert all(res1 == (par_dict.get("a", vals_in[0]) * x + par_dict.get("b.IDX1", vals_in[1])))
     assert all(res0 == (vals_in[0] * x + vals_in[1]))
     assert all(res2 == res0)
 
     savegraph(graph, f"{output_path}/{test_name}.png")
 
 
-@mark.parametrize("par_names,mapper", (
+@mark.parametrize("par_dict,mapper", (
     ({}, None),
     ({"parameters.all.a": 2.0, "parameters.all.b.IDX1": 3.0}, {"parameters.all.b.IDX1": "par1"}),
     ({"parameters.all.a": 2.0}, {"parameters.all.a": "par0"}),
     ({"parameters.all.b.IDX1": 3.0}, {"parameters.all.a": "par0"}),
 ))
 @mark.parametrize("pass_output", (False, True))
-def test_make_fcn_nonsafe(test_name, par_names, mapper, pass_output, output_path: str):
+def test_make_fcn_nonsafe(test_name, par_dict, mapper, pass_output, output_path: str):
     n = 10
     x = arange(n, dtype="d")
     vals_in = [1.0, 2.0]
@@ -98,30 +98,30 @@ def test_make_fcn_nonsafe(test_name, par_names, mapper, pass_output, output_path
         f.outputs[0] if pass_output else f,
         storage,
         mapper=mapper,
-        par_names=par_names,
+        par_dict=par_dict,
         safe=False,
     )
 
-    if par_names:
+    if par_dict:
         with raises(KeyError):
             LF(non_existent=12)
 
         with raises(RuntimeError):
-            LF(*[value for value in par_names.values()], non_existent=12)
+            LF(*[value for value in par_dict.values()], non_existent=12)
 
-    res1 = LF(*[value for value in par_names.values()])
+    res1 = LF(*[value for value in par_dict.values()])
     res2 = LF()
 
     if mapper:
-        res3 = LF(**{mapper.get(parname, parname): parvalue for parname, parvalue in par_names.items()})
+        res3 = LF(**{mapper.get(parname, parname): parvalue for parname, parvalue in par_dict.items()})
         assert all(res1 == res3)
 
     # pars equal new values
-    assert A.value == par_names.get("parameters.all.a", vals_in[0])
-    assert B.value == par_names.get("parameters.all.b.IDX1", vals_in[1])
+    assert A.value == par_dict.get("parameters.all.a", vals_in[0])
+    assert B.value == par_dict.get("parameters.all.b.IDX1", vals_in[1])
     # new result is the same as the result of LF
     assert all(res0c == (vals_in[0] * x + vals_in[1]))
-    assert all(res1 == (par_names.get("parameters.all.a", vals_in[0]) * x + par_names.get("parameters.all.b.IDX1", vals_in[1])))
+    assert all(res1 == (par_dict.get("parameters.all.a", vals_in[0]) * x + par_dict.get("parameters.all.b.IDX1", vals_in[1])))
     assert all(res1 == res0)
     assert all(res1 == res2)
 
